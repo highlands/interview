d6fdc9122201c2c52d4d3d84e15f96492e771e10
arinthros <arinthros@gmail.com>
2019-02-02 09:29:38 -0500

    Switched to bootstrap via webpacker

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -34,29 +34,39 @@ class Dashboard extends Component {
   render(){
     
     let autoCompleteList = this.state.autoCompleteResults.map((response, index) => {
-      return <div class="col-md-4" key={index}>
-        <h3>{response.name}</h3>
-        <p>UPC: {response.upc}</p>
-        <p>Available on: {response.available_on}</p>
-        <p>Properties:</p>
-        <ul>
-        {
-          response.product_properties.map((item, index) => {
-            return <li key={index}>{item.property}: {item.product_property_value}</li>
-          })
-        }
-        </ul>
-        
+      return <div className="col-md-4 product-col" key={index}>
+        <div className="card">
+          <h5 className="card-header">{response.name}</h5>
+          <div className="card-body">
+            <p className="card-text">UPC: {response.upc}</p>
+            <p className="card-text">Available on: {response.available_on}</p>
+            <p className="card-text">Properties:</p>
+            <ul className="list-group">
+            {
+              response.product_properties.map((item, index) => {
+                return <li key={index} className="list-group-item">{item.property}: {item.product_property_value}</li>
+              })
+            }
+            </ul>
+          </div>
+        </div>
       </div>
     })
     
     return (
-      <div class="container">
-        <input ref={ (input) => { this.searchBar = input } } value={ this.state.term } onChange={ this.getAutoCompleteResults.bind(this) } type="text" placeholder="Search..." />
-        <div class="row">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="form-group">
+              <input ref={ (input) => { this.searchBar = input } } value={ this.state.term } onChange={ this.getAutoCompleteResults.bind(this) } type="text" placeholder="Search..." className="form-control" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
         { autoCompleteList }
         </div>
       </div>
+      
     )
   }
 }
